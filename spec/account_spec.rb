6cb118b39d536d7dbf35a5b7b1f062cf835dcf18
Claudia Green <claudia@claudiag.me.uk>
2016-05-16 14:48:56 +0100

    2/2 tests passing. Account initialises with a balance.

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,1 +1,17 @@
 require 'account'
+
+describe Account do
+
+  subject(:account) {described_class.new}
+
+
+  describe '#balance' do
+
+    it{ expect(account).to respond_to(:balance) }
+
+    it 'is initialized with a balance of 0' do
+      expect(account.balance).to eq(0)
+    end
+
+  end
+end
