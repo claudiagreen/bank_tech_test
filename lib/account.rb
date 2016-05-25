972052e37c323dbf0cd92563be99ef2603abe03c
claudia@claudiag.me.uk <claudia@claudiag.me.uk>
2016-05-25 13:27:43 +0100

    Withdrawal added. User class deleted and transaction class created. 6/6 tests passing.

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,30 @@
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :history
 
-  def initialize
+  def initialize(transaction_klass: Transaction)
     @balance = 0
+    @history = []
+    @transaction_klass = transaction_klass
   end
+
+  def withdraw(amount)
+    update_balance(-amount)
+    save_transaction_history(-amount, @balance)
+  end
+
+
+
+
+private
+
+  def update_balance(amount)
+    @balance += amount
+  end
+
+  def save_transaction_history(amount, balance)
+    transaction = @transaction_klass.new(amount, balance)
+    @history << transaction
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,16 +2,43 @@ require 'account'
 
 describe Account do
 
-  subject(:account) {described_class.new}
+  subject(:account) {described_class.new(transaction_klass: transaction_klass)}
+
+  let(:transaction_klass) { double(:transaction_klass, new: transaction) }
+  let(:transaction) { double(:transaction) }
 
 
-  describe '#balance' do
+  describe '#initialize' do
 
     it{ expect(account).to respond_to(:balance) }
 
     it 'is initialized with a balance of 0' do
       expect(account.balance).to eq(0)
     end
+    it 'initializes with an empty history' do
+      expect(account.history).to eq []
+    end
+  end
+
+  describe '#withdraw' do
+
+  before(:each) do
+    account.withdraw(500)
+  end
+
+    it 'creates a transaction with negative integer as amount' do
+      expect(transaction_klass).to have_received(:new).with(-500, -500)
+    end
+
+    it 'subtract from the balance when withdrawal is made' do
+     expect(account.balance).to eq -500
+    end
+
+   it 'saves the transaction history in the array' do
+     last_transaction = account.history.last
+     expect(last_transaction).to eq transaction
+   end
 
   end
+
 end
